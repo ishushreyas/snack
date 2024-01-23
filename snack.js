0cbeaaf23f72aea48755e40d699d3564cd8bb484
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-01-23 19:40:33 +0530

    Update snack.js

--- snack.js
+++ snack.js
@@ -1,10 +1,13 @@
-export function snackbar(msg){
+export function initializeSnack(){
   var style = document.createElement('link');
   style.rel = "stylesheet";
   style.href = "https://ishushreyas.github.io/snack/style.css";
   document.head.appendChild(style);
+}
+export function snack(msg,interval=1000){
   var sb = document.createElement('div');
   document.body.appendChild(sb);
   sb.style = 'position : fixed; bottom: 50px;';
   sb.innerHTML = `<div class='snackbar'><p>${msg}</p><span class='material-symbols-outlined' onclick='this.parentElement.parentElement.remove()'>close</span></div>`;
+  setTimeout(function (){sb.remove()},interval);
 }
